00c15bbe3c39406eecce1d054e3b08413b5e0ad1
Jeremy Coulson <jeremycoulson@gmail.com>
2012-06-27 11:24:47 -0400

    Adding the files.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 Config-Encryptor
 ================
 
-Encrypts or decrypts sections of a web.config file.
+Encrypts or decrypts sections of a web.config file so you don't have to remember the command.
+
+Setup
+-----
+
+1. Set `aspnetpath` to the path to your ASP.NET directory.
+2. Set `site1` to the name of a site and `siteIDvar1` to that site's ID.  You can find that info in IIS.
+3. Repeat step 2 for every site on the machine.
+4. Add sites to the site names section as necessary.
+5. Add a conditional statement for each site.
+
+Future Revisions
+----------------
+
+This is clearly not useful if your machine hosts a ton of sites.  In the future, I would like to make it iterate through some collection of site names and IDs; at this point, however, the script works just perfectly for my small needs at work.
---
+++ encrypt-decrypt.bat
@@ -0,0 +1,40 @@
+@ECHO OFF
+
+mode con: lines=40
+mode con: cols=170
+
+ECHO ***********************************************************
+ECHO ** Encrypts or decrypts Web.config elements.             **
+ECHO **                                                       **
+ECHO ** Proceed with caution! For aspnet_regiis docs, see:    **
+ECHO ** http://msdn.microsoft.com/en-us/library/zhhddkxy.aspx **
+ECHO ***********************************************************
+ECHO.
+
+SET aspnetpath=%windir%\Microsoft.NET\Framework\v4.0.30319\
+SET site1=sitename
+SET siteIDvar1=123456789
+
+ECHO ** Site names on this machine:
+ECHO %site1%
+ECHO.
+
+ECHO Enter d to decrypt or e to encrypt:
+SET /p action= 
+ECHO Enter the section you wish to encrypt or decrypt:
+SET /p section= 
+ECHO Enter the application path:
+SET /p app= 
+ECHO Enter the site name for the application (see above):
+SET /p site= 
+ECHO.
+
+if %site%==%site1% SET siteID=%siteIDvar1%
+
+ECHO Running:
+ECHO %aspnetpath%aspnet_regiis.exe -p%action% "%section%" -app "%app%" -site "%siteID%"
+ECHO.
+%aspnetpath%aspnet_regiis.exe -p%action% "%section%" -app "%app%" -site "%siteID%"
+ECHO.
+
+pause
